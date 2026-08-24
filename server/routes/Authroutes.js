56a4e0940f42a9ba3b8f7c5046ad142e36d6f909
nidhi <modinidhi321@gmail.com>
2026-08-24 15:07:17 +0530

    Save local changes

--- server/routes/Authroutes.js
+++ server/routes/Authroutes.js
@@ -4,12 +4,19 @@ const router = express.Router();
 const { registerUser } = require("../controllers/authController");
 
 const { loginUser } = require("../controllers/authController");
-
+const {protect} = require("../middleware/Authmiddleware");
+const {updateProfile} = require("../controllers/authController");
+const {logoutUser} = require("../controllers/AuthController");
 //for registration
 // Public route
 router.post("/register", registerUser);
 
 // for log-in
-router.post("/login",loginUser);
+router.post("/login",loginUser)
+
+// //updateProfile;
+// router.put("/profile", protect, updateProfile);
+// //logoutUser
+// router.post("/logout",protect,logoutUser);
 
 module.exports = router;
